f3de84f69f967f69ed6deae060c1dcdad7f3b43f
Zerumi <evgexa.2015@bk.ru>
2019-11-13 22:34:30 +0400

    Created Example of API for interacting with server

--- BotNet API/app.js
+++ BotNet API/app.js
@@ -1,65 +1,33 @@
-﻿'use strict';
-var debug = require('debug');
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+﻿const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+app.set('port', PORT);
+app.set('env', NODE_ENV);
+app.use(logger('tiny'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+app.use('/', require(path.join(__dirname, 'routes')));
+app.use((req, res, next) => {
+    const err = new Error(`${req.method} ${req.url} Not Found`);
     err.status = 404;
     next(err);
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
+    console.error(err);
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+    res.json({
+        error: {
+            message: err.message,
+        },
     });
 });
-
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
+app.listen(PORT, () => {
+    console.log(
+        `Express Server started on Port ${app.get(
+            'port'
+        )} | Environment : ${app.get('env')}`
+    );
 });
